825e52a6c64c33e9a3faf13d4c25053983e5b0d3
shizuwei99 <61020380+shizuwei99@users.noreply.github.com>
2020-11-03 13:16:45 +0800

    Add files via upload

--- source/js/FunnyTitle.js
+++ source/js/FunnyTitle.js
@@ -3,12 +3,12 @@
  var titleTime;
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
-         $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/Yafine/cdn@2.5/source/favicon.png");
+         $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/Yafine/cdn@2.2/source/favicon.png");
          document.title = 'ヽ(●-`Д´-)ノ你要玩捉迷藏嘛';
          clearTimeout(titleTime);
      }
      else {
-         $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/Yafine/cdn@2.5/source/favicon.png");
+         $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/Yafine/cdn@2.2/source/favicon.png");
          document.title = 'ヾ(Ő∀Ő3)ノ好哦！' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
